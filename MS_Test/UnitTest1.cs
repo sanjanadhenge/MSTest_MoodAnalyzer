7d8034ff2fff3ac46a42ff2be2b5256135d3aa50
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-01 09:38:27 +0530

    [sanjana] Add : Analyze Mood Sad and Any

--- MS_Test/UnitTest1.cs
+++ MS_Test/UnitTest1.cs
@@ -1,16 +1,28 @@
+using MAnalyzer;
+
 namespace MS_Test
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        //TC1.1
+       [Test]
+        public void GivenSadMood_WhenAnalyze_shouldReturnSad()
         {
-        }
+            string message = "I am in Sad Mood";
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            string result1 = moodAnalyzer.AnalyzeMood(message);
 
+            Assert.AreEqual("SAD", result1);
+        }
+        //TC1.2
         [Test]
-        public void Test1()
+        public void GivenAnyMood_WhenAnalyze_shouldReturnSad()
         {
-            Assert.Pass();
+            string message = "I am in Any Mood";
+            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+            string result = moodAnalyzer.AnalyzeMood(message);
+
+            Assert.AreEqual("HAPPY", result);
         }
     }
 }
