89ac032e5faae2b567c622f1d5be7d61b4594934
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-04-01 09:24:23 +0530

    [sanjana] Add : MoodAnalyzer

---
+++ MAnalyzer/MoodAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿namespace MAnalyzer
+{
+    public class MoodAnalyzer
+    {
+        public string AnalyzeMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
+        
+    }
+}
